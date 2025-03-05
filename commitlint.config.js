4a5b077e2f536063cb4e84facb75a92c69d086f1
hpgbao2204 <hpgbao@gmail.com>
2025-03-05 18:51:25 +0700

    fead: add base fe

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
-export default {
+module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['feat', 'fixbug', 'setup', 'release', 'fix', 'refactor', 'doc']],
     'type-case': [2, 'always', 'lower-case'],
   },
-}
+};
