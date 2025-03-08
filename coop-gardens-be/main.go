2b685b5867fa44cd67bb504e57487e8f777d32cd
hpgbao2204 <hpgbao@gmail.com>
2025-03-08 07:20:23 +0700

    setup: swagger

--- coop-gardens-be/main.go
+++ coop-gardens-be/main.go
@@ -4,25 +4,45 @@ import (
 	"log"
 
 	"coop-gardens-be/config"
+	_ "coop-gardens-be/docs"
 	"coop-gardens-be/internal/api/routers"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
 func main() {
-	// Load biến môi trường từ .env
+	// Load .env
 	err := godotenv.Load()
+
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	e := echo.New()
 
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
 	// Kết nối DB
 	config.InitDB()
 	// Khởi tạo routes
 	routers.LoginRoutes(e)
 
+	log.Println("🚀 Server đang chạy tại: http://localhost:8080")
 	e.Start(":8080")
 }
